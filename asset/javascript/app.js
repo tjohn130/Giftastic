452a76c34b996ca1969b71503ec374106ce71971
Teddy Johnson <tjohn13@email.cpcc.edu>
2018-11-01 17:13:56 -0400

    adding to app.js

--- asset/javascript/app.js
+++ asset/javascript/app.js
@@ -1,12 +1,69 @@
-var topic = ["Bunny","Bears","Jojo","Nature","Anime","Food","Desserts","Candy","Movies","Spiderman","Parkour","Japan"]
+$(document).ready(function(){
+    var topic = ["Bunny","Bears","Jojo","Nature","Anime","Food","Desserts","Candy","Movies","Spiderman","Parkour","Japan"]
+
+    function pullButton(){
+        
+        for(var i=0;i<topic.length;i++){
+
+            var newButton = $("<button>")
+            newButton.attr("data-number", i)
+            .addClass("topicButton")
+            .text(topic[i]);
+
+            $("#button").append(newButton);
+        }
+    }
+
+    pullButton();
+
+    $(".topicButton").click(function(){
+
+        var queryParam = {api_key: "19SOKo8StiKRg3T028H9ycDkZIe4Ssel"}
+        queryParam.q = $(this).text();
+        queryParam.limit = "10";
+        queryParam.rating = "g";
+        urlquery = "https://api.giphy.com/v1/gifs/search?"+$.param(queryParam);
+
+            $.ajax({
+                url: urlquery,
+                method: "GET",
+            }).then(function(response){
+            
+                console.log(response)
+                console.log(response.data[0].source_tld)
+
+                for(var j=0;j<response.data.length;j++){
+                    var imgsrcStill = response.data[j].images.original_still.url
+                    var imgsrcAnimate = response.data[j].images.original.url
+                    console.log(imgsrcStill)
+                    console.log(imgsrcAnimate)
+                    var newimg = $("<img>")
+                    newimg.addClass("topicPicture")
+                    .attr("src", imgsrcStill)
+                    .attr("data-loop", false)
+                    .attr("data-still", imgsrcStill)
+                    .attr("data-animate",imgsrcAnimate);
+                    $("#picture").prepend(newimg);
+                }
+            })  
+            
+    })
+
+    $("#picture").on("click",".topicPicture",function(){
+        var loopCheck = $(this).attr("data-loop");
+        var imgStill = $(this).attr("data-still");
+        var imgAnimate = $(this).attr("data-animate");
+        console.log(loopCheck)
+        console.log(imgAnimate+imgStill)
+        //Stop
+        if(loopCheck == false){
+            $(this).attr("src", imgAnimate);
+            console.log($(this).attr("src", imgAnimate))
 
 
-
-urlquery = "https://api.giphy.com/v1/gifs/search?q="+topic[0]+"&limit=20&rating=g&api_key=19SOKo8StiKRg3T028H9ycDkZIe4Ssel";
-
-$.ajax({
-    url: urlquery,
-    method: "GET",
-}).then(function(response){
+        }
+        
+        
+    })
 
 })
